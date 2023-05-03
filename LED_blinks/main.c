f7ef24424e4cfce5f4bc3a2beeb66bc6f8515359
ivukasin <vukasinivankovic6@gmail.com>
2023-05-03 14:10:25 +0200

    ledBlink function added

--- LED_blinks/main.c
+++ LED_blinks/main.c
@@ -6,7 +6,7 @@ int16_t main ()
 int16_t high_time = 300;
 int16_t low_time = 700;
 DDRB |= 1 << 5; // PB5 je izlaz
-while (1)
+void ledBlink ( int16_t high_time , int16_t low_time )
 {
 PORTB |= 1 << 5; // LED ON
 _delay_ms ( high_time ) ; // Pauza 1 s
